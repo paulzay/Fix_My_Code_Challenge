73849d173bbf0278f06f2719879bd64d559ce846
Paul Ogolla <paulotieno2@gmail.com>
2024-05-28 11:55:49 +0300

    fix lint

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+"""
+Square class
+"""
 
-class square():
-    
+
+class Square():
+    """ class definition"""
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """initialize values"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,14 +19,17 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """square perimeter"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """string version"""
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
